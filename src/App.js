7e944c395789de37da64027a1bd3ec451473b60a
TJ Yquin <tahnee.yquin@gmail.com>
2021-05-12 23:45:32 -0400

    Create app skeleton
    Add fonts, global styles and / and /search routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import './global-styles/global-styles.css';
+import './global-styles/normalize.css';
+
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+
+        <Switch>
+          <Route path="/search" />
+          <Route path="/how-it-works" />
+          <Route path="/about" />
+          <Route path="/" />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
